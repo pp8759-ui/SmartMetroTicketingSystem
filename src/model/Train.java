8c2c146fb2bcd39342c1040d890051b0902e05b8
pp8759-ui <pp8759@1utar.my>
2026-07-13 21:42:33 +0800

    finished Train.java and trainData.json

--- src/model/Train.java
+++ src/model/Train.java
@@ -1,4 +1,35 @@
 package model;
 
 public class Train {
+
+    private String trainId;
+    private String trainName;
+    private int capacity;
+
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public String getTrainName() {
+        return trainName;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public Train(String aTrainId, String aTrainName, int aCapacity) {
+        trainId = aTrain
+        trainName = aTrainName;
+        capacity = aCapacity;
+    }
+
+    public void displayTrain() {
+        System.out.println("Train ID: " + trainId);
+        System.out.println("Train Name: " + trainName);
+        System.out.println("Capacity: " + 0 + "/" + capacity);
+
+        // zero is placeholder, use to test
+        // later need to change to the total number of passenger already book this train
+    }
 }
